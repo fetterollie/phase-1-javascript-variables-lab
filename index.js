e44bb260509feb7f6d0c90c0a31b946c36934f8a
Jonathan <jonathan.fetterolf@gmail.com>
2023-05-17 06:16:30 -0400

    index updated passing npm tests

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solution in this file!
+// companyName
+// 1) is set as Scuber
+// 2) is defined as a const
+const companyName = 'Scuber'
+// mostProfitableNeighborhood
+// 3) is declared as equal to Chelsea
+// 4) is defined using let
+let mostProfitableNeighborhood = 'Chelsea'
+// companyCeo
+// 5) is declared as equal to Susan Smith
+// 6) is defined using let
+let companyCeo = 'Susan Smith'
